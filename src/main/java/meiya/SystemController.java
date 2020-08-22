b37be287b782c7be9333dcff07cdd492d6a2e416
kouxl <671817471@qq.com>
2020-08-22 17:40:40 +0800

    项目初始化

---
+++ src/main/java/meiya/SystemController.java
@@ -0,0 +1,12 @@
+package meiya;
+
+/**
+ * @ClassName SystemController
+ * @Author kouxl
+ * @Date 2020/8/22 2:30 PM
+ */
+public class SystemController {
+    public static void main(String[] args) {
+        System.out.println("Git项目创建！！");
+    }
+}
